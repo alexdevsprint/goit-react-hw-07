c60d303f99145b76488633dce9f9d865228d0314
alexdevsprint <alexdevsprint@gmail.com>
2025-06-03 16:32:53 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useEffect } from "react";
 import css from "./App.module.css";
 
+import Loader from "./components/Loader/Loader";
+import Error from "./components/Error/Error";
+
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchContacts } from "./redux/contactsOps";
 
 export default function App() {
   const dispatch = useDispatch();
+  const loading = useSelector((state) => state.contacts.loading);
+  const error = useSelector((state) => state.contacts.error);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -19,6 +24,8 @@ export default function App() {
       <h1 className={css.title}>Phonebook</h1>
       <ContactForm />
       <SearchBox />
+      {loading && <Loader />}
+      {error && <Error />}
       <ContactList />
     </div>
   );
