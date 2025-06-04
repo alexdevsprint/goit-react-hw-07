0e12148aa18b642d18c68eaac635c36b405986ad
alexdevsprint <alexdevsprint@gmail.com>
2025-06-04 15:50:24 +0300

    commit

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createSelector } from "@reduxjs/toolkit";
 import { fetchContacts, addContact, deleteContact } from "./contactsOps";
 import { selectNameFilter } from "./filtersSlice";
 const slice = createSlice({
@@ -65,11 +65,21 @@ export const selectLoading = (state) => state.contacts.loading;
 export const selectError = (state) => state.contacts.error;
 
 //Складні селектори
-export const selectFilteredContacts = (state) => {
-  const contacts = selectContacts(state);
-  const filter = selectNameFilter(state);
+// export const selectFilteredContacts = (state) => {
+//   const contacts = selectContacts(state);
+//   const filter = selectNameFilter(state); 
 
-  return contacts.filter((contact) =>
+//   return contacts.filter((contact) =>
+//     contact.name.toLowerCase().includes(filter.toLowerCase())
+//   );
+// };
+//Мемоізовані складні селектори
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectNameFilter],
+  (contacts, filter) => {
+    console.log("Мемоізований селектор")
+    return contacts.filter((contact) =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
   );
-};
+  }
+);
